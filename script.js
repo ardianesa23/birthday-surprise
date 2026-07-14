394dadb942bd844fd35daea8eef25a8edf517934
ardianesa23 <ardianesa23@gmail.com>
2026-07-14 21:31:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function bukaHadiah() {
-  alert("Selamat ulang tahun sayangku Liliana 🤍");
+  document.querySelector(".hero").innerHTML = `
+    <h1>Untuk Liliana Nur Hanifah 🤍</h1>
+
+    <p>
+    Selamat ulang tahun sayangku 🤍<br><br>
+    Terima kasih sudah menjadi seseorang yang spesial.
+    Semoga selalu bahagia, sehat, dan semua impian kamu tercapai.
+    </p>
+
+    <h2>Love you 🤍</h2>
+  `;
 }
