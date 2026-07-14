037056153c3703854703628638477473b0d05601
ardianesa23 <ardianesa23@gmail.com>
2026-07-15 01:39:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,32 +8,27 @@ function bukaHadiah() {
     }
 
     document.querySelector(".hero").innerHTML = `
-    ...
+
+        <img src="IMG-20251016-WA0006.jpg" class="foto">
+
+        <h1 class="nama">
+            Hello Liliana Nur Hanifah 🤍
+        </h1>
+
+        <p class="ucapan">
+            Selamat ulang tahun, Sayangg 🤍<br><br>
+            Semoga kamu selalu diberikan kesehatan,
+            kebahagiaan, dan semua hal baik yang kamu impikan.<br><br>
+            Terima kasih sudah menjadi seseorang yang
+            sangat berarti dalam hidupku 🤍
+        </p>
+
+        <button class="pesan-btn" onclick="pesanTerakhir()">
+            Ada satu pesan lagi 🤍
+        </button>
+
     `;
-}
 
-    <img src="IMG-20251016-WA0006.jpg" class="foto">
-
-
-    <h1 class="nama">
-      Hello Liliana Nur Hanifah 🤍
-    </h1>
-
-
-    <p class="ucapan">
-      Selamat ulang tahun, Sayangg 🤍<br><br>
-      Semoga kamu selalu diberikan kesehatan,
-      kebahagiaan, dan semua hal baik yang kamu impikan.<br><br>
-      Terima kasih sudah menjadi seseorang yang
-      sangat berarti dalam hidupku 🤍
-    </p>
-
-
-    <button class="pesan-btn" onclick="pesanTerakhir()">
-      Ada satu pesan lagi 🤍
-    </button>
-
-  `;
 }
 
 function pesanTerakhir() {
