f80aac357a860eef8b03cc3b765e2119d68f1a79
ardianesa23 <ardianesa23@gmail.com>
2026-07-14 21:37:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,33 @@ function bukaHadiah() {
 
     <p>
     Selamat ulang tahun sayangku 🤍<br><br>
-    Terima kasih sudah menjadi seseorang yang spesial.
-    Semoga selalu bahagia, sehat, dan semua impian kamu tercapai.
+    Terima kasih sudah hadir dan menjadi seseorang yang spesial.
+    Semoga di umur yang baru ini kamu selalu diberikan kebahagiaan,
+    kesehatan, dan semua impian kamu bisa tercapai.
     </p>
 
     <h2>Love you 🤍</h2>
   `;
+
+  buatHati();
+}
+
+function buatHati() {
+  setInterval(() => {
+    let hati = document.createElement("div");
+    hati.innerHTML = "🤍";
+
+    hati.style.position = "fixed";
+    hati.style.left = Math.random() * 100 + "vw";
+    hati.style.bottom = "-20px";
+    hati.style.fontSize = "25px";
+    hati.style.animation = "naik 5s linear";
+
+    document.body.appendChild(hati);
+
+    setTimeout(() => {
+      hati.remove();
+    }, 5000);
+
+  }, 300);
 }
